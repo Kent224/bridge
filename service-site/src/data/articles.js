ee4d72bfa106bb30402c5f3ab425ffba6b774efe
Kent224 <ken.w.0920@gmail.com>
2025-04-14 17:41:19 +0900

    fix: 全ての記事ファイルをインポートするように修正

--- service-site/src/data/articles.js
+++ service-site/src/data/articles.js
@@ -5,7 +5,29 @@ import { article4 } from './articles/article4';
 import { article5 } from './articles/article5';
 import { article6 } from './articles/article6';
 import { article7 } from './articles/article7';
+import { article8 } from './articles/article8';
+import { article9 } from './articles/article9';
+import { article10 } from './articles/article10';
+import { article11 } from './articles/article11';
+import { article12 } from './articles/article12';
+import { article13 } from './articles/article13';
+import { article14 } from './articles/article14';
+import { article15 } from './articles/article15';
+import { article16 } from './articles/article16';
+import { article17 } from './articles/article17';
+import { article18 } from './articles/article18';
+import { article19 } from './articles/article19';
+import { article20 } from './articles/article20';
+import { article21 } from './articles/article21';
+import { article22 } from './articles/article22';
+import { article23 } from './articles/article23';
+import { article24 } from './articles/article24';
+import { article25 } from './articles/article25';
+import { article26 } from './articles/article26';
 import { article27 } from './articles/article27';
+import { article28 } from './articles/article28';
+import { article29 } from './articles/article29';
+import { article30 } from './articles/article30';
 
 export const articles = [
   article1,
@@ -15,5 +37,27 @@ export const articles = [
   article5,
   article6,
   article7,
-  article27
+  article8,
+  article9,
+  article10,
+  article11,
+  article12,
+  article13,
+  article14,
+  article15,
+  article16,
+  article17,
+  article18,
+  article19,
+  article20,
+  article21,
+  article22,
+  article23,
+  article24,
+  article25,
+  article26,
+  article27,
+  article28,
+  article29,
+  article30
 ]; 
